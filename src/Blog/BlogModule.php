4352a63df3a7aa416e6a92561e60ce2c57e70646
JozBLT <jonjon.joz114@gmail.com>
2024-04-21 21:22:01 +0200

    add basic renderer system and demo views

--- src/Blog/BlogModule.php
+++ src/Blog/BlogModule.php
@@ -2,6 +2,7 @@
 
 namespace App\Blog;
 
+use Framework\Renderer;
 use Framework\Router;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -9,19 +10,27 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 class BlogModule
 {
 
-    public function __construct(Router $router)
+    private Renderer $renderer;
+
+    public function __construct(Router $router, Renderer $renderer)
     {
+        $this->renderer = $renderer;
+        $this->renderer->addPath('blog', __DIR__ . '/views');
         $router->get('/blog', [$this, 'index'], 'blog.index');
         $router->get('/blog/[*:slug]', [$this, 'show'], 'blog.show');
     }
 
-    public function index(Request $request): string
+    public function index(Request $request): Response
     {
-        return '<h1>Bienvenue sur le blog</h1>';
+        $content = $this->renderer->render('@blog/index');
+        return new \GuzzleHttp\Psr7\Response(200, [], $content);
     }
 
-    public function show(Request $request): string
+    public function show(Request $request): Response
     {
-        return '<h1>Bienvenue sur l\'article ' . $request->getAttribute('slug') . '</h1>';
+        $content = $this->renderer->render('@blog/show', [
+            'slug' => $request->getAttribute('slug')
+        ]);
+        return new \GuzzleHttp\Psr7\Response(200, [], $content);
     }
 }
